891934ec039f97a1e75ccb6556027ebeb875270e
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-22 02:03:27 +0530

    Default controller

---
+++ src/main/java/com/example/oauthimplementationspringboot/OauthImplementationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthimplementationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthImplementationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthImplementationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oauthimplementationspringboot/OauthImplementationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthimplementationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthImplementationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
